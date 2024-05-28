f7d00deaf4453759a79f980b27414089093d83a1
Aman <aman.proj.rel@gmail.com>
2024-05-28 14:11:04 +0530

    implement comboBox backend

--- src/settings/elements/common/combobox_template.py
+++ src/settings/elements/common/combobox_template.py
@@ -3,12 +3,44 @@ import os
 from kivy.lang import Builder
 from kivy.config import ConfigParser
 from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.menu import MDDropdownMenu
+from kivymd.app import MDApp
 
 kivy_design_files = ["combobox_template"]
 for kv_file in kivy_design_files:
     Builder.load_file(os.path.join(os.path.abspath(os.getcwd()), "src", "settings", "elements", "common", kv_file + ".kv"))
 
 class comboBox(MDBoxLayout):
+    drop_item_menu: MDDropdownMenu = None
+
     def __init__(self, config: ConfigParser, section_name, setting_name, options, title, **kwargs):
         MDBoxLayout.__init__(self, **kwargs)
         self.config = config
+        self.section_name = section_name
+        self.setting_name = setting_name
+        self.options = options
+        self.title = title
+
+        self.ids.label_option_name.text = self.title
+        self.ids.button_select.text = self.config.get(section_name, setting_name)
+
+    def open_drop_item_menu(self, item):
+        menu_items = [
+            {
+                "text": f"{self.options[i]}",
+                "on_release": lambda x=f"{self.options[i]}": self.menu_button_clicked(x),
+            }
+            for i in range(len(self.options))
+        ]
+
+        if not self.drop_item_menu:
+            self.drop_item_menu = MDDropdownMenu(caller=item, items=menu_items, position="center")
+        
+        self.drop_item_menu.open()
+
+    def menu_button_clicked(self, item_text):
+        self.ids.button_select.text = item_text
+        self.drop_item_menu.dismiss()
+        self.config[self.section_name][self.setting_name] = item_text
+        self.config.write()
+        MDApp.get_running_app().apply_settings_to_app()
--- src/settings/elements/sub_settings/look_and_feel.py
+++ src/settings/elements/sub_settings/look_and_feel.py
@@ -1,9 +1,20 @@
 from src.settings.elements.common.sub_settings_template import subSettingsTemplate
 
+import json
+
 from kivy.config import ConfigParser
 
 class lookAndFeel(subSettingsTemplate):
     def __init__(self, config: ConfigParser, title = "No title provided", **kwargs):
         super(lookAndFeel, self).__init__(config, title, **kwargs)
-        self.add_combo_box("section_name", "setting_name", "options", "title")
-        self.add_combo_box("section_name", "setting_name", "options", "title")
+
+        with open("available_options_for_each_setting.json", 'r') as json_file:
+            available_options = json.load(json_file)
+
+        look_and_feel_options  = available_options["look and feel"]
+        
+        self.clear_all_available_settings()
+        
+        self.add_combo_box("look and feel", "primary-palette", look_and_feel_options["primary-palette"], "Primary Palette")
+        self.add_combo_box("look and feel", "accent-palette", look_and_feel_options["accent-palette"], "Accent Palette")
+        self.add_combo_box("look and feel", "theme", look_and_feel_options["theme"], "Theme")
