c1c7386680f4eaa808da371f754c54bb969e87c1
Aman <aman.proj.rel@gmail.com>
2024-06-19 12:33:20 +0530

    add tabs feature in settings

--- src/settings/elements/common/sub_settings_template.py
+++ src/settings/elements/common/sub_settings_template.py
@@ -8,13 +8,26 @@ from kivy.lang import Builder
 from kivy.config import ConfigParser
 from kivy.resources import resource_find
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.tab import MDTabsBase
+from kivymd.uix.floatlayout import MDFloatLayout
 
 kivy_design_files = ["sub_settings_template"]
 for kv_file in kivy_design_files:
     Builder.load_file(os.path.join(os.path.dirname(os.path.realpath( __file__ )), kv_file + ".kv"))
 
+class Tab(MDFloatLayout, MDTabsBase):
+    """Class implementing content for a tab."""
+    id:str = None
+    def __init__(self, id:str, **kwargs):
+        self.id = id
+        super(Tab, self).__init__(**kwargs)
+
 class subSettingsTemplate(MDScreen):
     sub_settings_list = []
+    show_tabs_view = False
+    tabs_info_dict = {}
+    tabs_instances = []
+
     def __init__(self, config: ConfigParser, setting_section_name:str, **kwargs):
         MDScreen.__init__(self, **kwargs)
         self.config = config
@@ -26,7 +39,43 @@ class subSettingsTemplate(MDScreen):
     
         self.ids.top_bar.title = self.setting_properties["title"]
 
+        number_of_sub_settings_with_tabs = 0
+        for sub_setting_data_key in self.setting_properties["data"].keys():
+            if "tab" in self.setting_properties["data"][sub_setting_data_key]:
+                number_of_sub_settings_with_tabs += 1
+                if self.setting_properties["data"][sub_setting_data_key]["tab"]["id"] not in self.tabs_info_dict.keys():
+                    self.tabs_info_dict[self.setting_properties["data"][sub_setting_data_key]["tab"]["id"]] = self.setting_properties["data"][sub_setting_data_key]["tab"]
+        if len(self.setting_properties["data"]) == number_of_sub_settings_with_tabs and 0 != number_of_sub_settings_with_tabs:
+            self.show_tabs_view = True
+        elif len(self.setting_properties["data"]) != number_of_sub_settings_with_tabs and 0 != number_of_sub_settings_with_tabs:
+            #! all sub-settings doesn't have tab names
+            raise AttributeError(f"\"tab\" is assigned to only some members of the setting {self.ids.top_bar.title}.\nNumber of tabs are {number_of_sub_settings_with_tabs}.\nNumber of sub-settings are {len(self.setting_properties["data"])}.")
+        
+        if self.show_tabs_view:
+            #* create tabs
+            self.remove_widget(self.ids.scroll_view_for_settings)
+            for tab in self.tabs_info_dict.values():
+                tab_elements = tab.keys()
+                if "icon" in tab_elements and "name" not in tab_elements:
+                    self.tabs_instances.append(Tab(id = tab["id"],icon = tab["icon"]))
+                    self.ids.tabs.add_widget(self.tabs_instances[-1])
+                elif "icon" in tab_elements and "name" in tab_elements:
+                    self.tabs_instances.append(Tab(id = tab["id"],icon = tab["icon"], title = tab["name"]))
+                    self.ids.tabs.add_widget(self.tabs_instances[-1])
+                elif "icon" not in tab_elements and "name" in tab_elements:
+                    self.tabs_instances.append(Tab(id = tab["id"],title = tab["name"]))
+                    self.ids.tabs.add_widget(self.tabs_instances[-1])
+
+        else:
+            self.tabs_info_dict = []
+            self.remove_widget(self.ids.tabs)
+
     def clear_all_available_settings(self):
+        if self.show_tabs_view:
+            for tab in self.tabs_instances:
+                tab.ids.sub_settings.clear_widgets()
+            return self
+        
         self.ids.sub_settings.clear_widgets()
         return self
     
@@ -37,10 +86,14 @@ class subSettingsTemplate(MDScreen):
                              self.setting_properties["data"][setting_name_in_settings_properties_json_file]["options"],
                              self.setting_properties["data"][setting_name_in_settings_properties_json_file]["title"],
                              **kwargs)
-        self.sub_settings_list.append(combo_box)
-        self.ids.sub_settings.add_widget(combo_box)
+
+        self.add_element_to_settings_screen(combo_box,
+                                            self.setting_properties["data"][setting_name_in_settings_properties_json_file]["tab"]["id"] if "tab" in self.setting_properties["data"][setting_name_in_settings_properties_json_file].keys() else None)
 
     def highlight_setting(self, section_name: str, setting_name: str):
+        if self.show_tabs_view:
+            # todo switch to tab with that particular setting
+            pass
         for sub_setting in self.sub_settings_list:
             if (sub_setting.section_name == section_name) and (sub_setting.setting_name == setting_name):
                 sub_setting.animate_for_easy_navigation()
@@ -52,8 +105,9 @@ class subSettingsTemplate(MDScreen):
                              self.setting_properties["data"][setting_name_in_settings_properties_json_file]["title"],
                              self.setting_properties["data"][setting_name_in_settings_properties_json_file]["description"],
                              **kwargs)
-        self.sub_settings_list.append(info_label)
-        self.ids.sub_settings.add_widget(info_label)
+
+        self.add_element_to_settings_screen(info_label,
+                                            self.setting_properties["data"][setting_name_in_settings_properties_json_file]["tab"]["id"] if "tab" in self.setting_properties["data"][setting_name_in_settings_properties_json_file].keys() else None)
 
         return self
 
@@ -76,6 +130,7 @@ class subSettingsTemplate(MDScreen):
             self.clear_all_available_settings()
 
         settings_not_added = []
+
         for setting_name_in_settings_properties_json_file in self.setting_properties["data"]:
             if self.setting_properties["data"][setting_name_in_settings_properties_json_file]["type"] == "combo box":
                 self.add_combo_box(setting_name_in_settings_properties_json_file, **kwargs)
@@ -86,3 +141,11 @@ class subSettingsTemplate(MDScreen):
         
         return self, settings_not_added
 
+    def add_element_to_settings_screen(self, element, tab_id = None):
+        self.sub_settings_list.append(element)
+        if not self.show_tabs_view:
+            self.ids.sub_settings.add_widget(element)
+        else:
+            for tab in self.tabs_instances:
+                if tab.id == tab_id:
+                    tab.ids.sub_settings.add_widget(element)
