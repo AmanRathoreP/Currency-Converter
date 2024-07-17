AmanRathoreP/Currency-Converter

commit 9cd485e42842ea9bab061662e9def11549dcabc3

add all the non-custom currencies to the ui

src/settings/elements/common/checkbox_template.py
@@ -54,7 +54,7 @@ class customCheckBox(individualSettingBaseClass):
         self.all_options_select_title = all_options_select_title
 
         if self.check_box_type == "boolean":
-            self.ids.boolean.state = "down" if self.config[self.section_name][self.setting_name] == "True" else "normal"
+            self.ids.boolean.state = "down" if self.previous_boolean_state_of_check_box == "True" else "normal"
             self.remove_widget(self.ids.check_box_with_parent_container)
         elif self.check_box_type == "multiple-options-select":
             self.remove_widget(self.ids.boolean)
@@ -89,3 +89,7 @@ class customCheckBox(individualSettingBaseClass):
     
     def show_ripple(self, show_ripple: bool = True):
         self.ripple_behavior = show_ripple
+    
+    @property
+    def previous_boolean_state_of_check_box(self):
+        self.config[self.section_name][self.setting_name]

src/settings/elements/common/checkbox_template_for_currencies_select.py
@@ -1,0 +1,43 @@
+from src.settings.elements.common.checkbox_template import customCheckBox
+
+from kivymd.app import MDApp
+
+class customCheckBoxForCurrency(customCheckBox):
+    def on_box_clicked(self):
+        
+        try:
+            currently_active_non_custom_currencies = str(self.config[self.section_name]["active-non-custom-currencies"]).replace('\'', '').replace(' ', '')[1:-1].split(',')
+        except:
+            print("\"active-non-custom-currencies\" not found in app settings ini file!")
+            return
+        
+        if self.ids.boolean.state == "down":
+            #* include currency
+            if self.setting_name in currently_active_non_custom_currencies:
+                return
+            else:
+                currently_active_non_custom_currencies.append(self.setting_name)
+        else:
+            #* exclude currency
+            if self.setting_name in currently_active_non_custom_currencies:
+                currently_active_non_custom_currencies.remove(self.setting_name)
+            else:
+                return
+
+        
+        self.config[self.section_name]["active-non-custom-currencies"] = str(currently_active_non_custom_currencies)
+        self.config.write()
+        MDApp.get_running_app().apply_settings_to_app()
+    
+    @property
+    def previous_boolean_state_of_check_box(self):
+        try:
+            currently_active_non_custom_currencies = str(self.config[self.section_name]["active-non-custom-currencies"]).replace('\'', '').replace(' ', '')[1:-1].split(',')
+        except:
+            print("\"active-non-custom-currencies\" not found in app settings ini file!")
+            return
+        
+        if self.setting_name in currently_active_non_custom_currencies:
+            return "True"
+        return "False"
+        

src/settings/elements/sub_settings/currencies_to_include.py
@@ -1,6 +1,8 @@
 from src.settings.elements.common.sub_settings_template import subSettingsTemplate
 from src.settings.elements.sub_settings.currency_converter import ExchangeRates
+from src.settings.elements.common.checkbox_template_for_currencies_select import customCheckBoxForCurrency
 
+from kivy.resources import resource_find
 from kivy.config import ConfigParser
 
 class Currency(subSettingsTemplate):
@@ -9,4 +11,25 @@ class Currency(subSettingsTemplate):
         
         self.clear_all_available_settings()
 
-        self.add_all_possible_settings()
+        try:
+            er = ExchangeRates(resource_find("exchange_rates.json"))
+        except RuntimeError as e:
+            # todo so info to user
+            print(e)
+        
+        curr_list = er.get_lists()
+        for type in curr_list:
+            for cr in curr_list[type]:
+                print(f"{cr.upper()}, {curr_list[type][cr]["name"]}")
+
+                check_box = customCheckBoxForCurrency(
+                    self.config,
+                    "currencies to include",
+                    cr.upper(),
+                    f"{cr.upper()}, {curr_list[type][cr]["name"]}",
+                    check_box_type  = "boolean",
+                    **kwargs
+                    )
+                for tab in self.tabs_instances:
+                    if tab.id == "available currencies by default":
+                        tab.ids.sub_settings.add_widget(check_box)

commit d51ad2d02239e3ccdcb3664361c34cf0ed03b2e8

bug fix of checkbox-boolean wrong previous state
this bug was introduced in commit 9cd485e42842ea9bab061662e9def11549dcabc3

src/home/home.py
@@ -95,7 +95,7 @@ class homeScreen(Screen):
     def __load_self_configuration_data(self):
         self.__number_format_system = str(self.config["format numbers' looks"]["number-format-system"])
         self.__decimal_places_to_show = int(self.config["format numbers' looks"]["decimal-precision"])
-        self.__smart_formatting = bool(self.config["format numbers' looks"]["smart-formatting"])
+        self.__smart_formatting = self.config["format numbers' looks"]["smart-formatting"] == "True"
 
         if self.__number_format_system == "International Number System":
             self.__number_format_system = "global"

src/settings/elements/common/checkbox_template.py
@@ -92,4 +92,4 @@ class customCheckBox(individualSettingBaseClass):
     
     @property
     def previous_boolean_state_of_check_box(self):
-        self.config[self.section_name][self.setting_name]
+        return self.config[self.section_name][self.setting_name]